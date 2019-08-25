166fba3a8bd5734b6f1511c5612ac2d039bf57da
Meari <meari.jung@gmail.com>
2019-08-25 15:38:19 +0900

    eawt
    eat

--- forvide.py
+++ forvide.py
@@ -1,14 +1,1 @@
-import unittest
-
-class ForVideo(unittest.textCase):
-    def setUp(self):
-        print 'some text'
-
-    def test_search_in_python_org(self):
-        print 'some text'
-
-    def tearDown(self):
-        print 'some txt'
-
-if __name__ == "__main__":
-    unittest.main()
+print('hello, world')
