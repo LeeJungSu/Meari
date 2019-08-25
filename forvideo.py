dca4e905b4cb7f4bf001e377b4a3d25982ad0641
Meari <meari.jung@gmail.com>
2019-08-25 15:30:11 +0900

    test
    ttest

---
+++ forvideo.py
@@ -0,0 +1,14 @@
+import unittest
+
+class ForVideo(unittest.textCase):
+    def setUp(self):
+        print 'some text'
+
+    def test_search_in_python_org(self):
+        print 'some text'
+
+    def tearDown(self):
+        print 'some txt'
+
+if __name__ == "__main__":
+    unittest.main()
